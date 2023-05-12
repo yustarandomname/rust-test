58ecdbd06f52e087f0d792c9318601b2e53d0809
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-05-12 10:05:04 +0200

    Have propper serialised version

--- graph_walker/src/lib.rs
+++ graph_walker/src/lib.rs
@@ -4,11 +4,7 @@ use hyper_params::HyperParams;
 use pad::PadStr;
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
-use std::{
-    collections::{HashMap, HashSet},
-    f32::consts::E,
-    fmt,
-};
+use std::{collections::HashMap, f32::consts::E, fmt};
 
 const HYPER_PARAMS: HyperParams = HyperParams {
     gamma: 0.5,
@@ -24,58 +20,48 @@ enum AgentSpecies {
 
 const AGENT_SPECIES: [AgentSpecies; 2] = [AgentSpecies::Red, AgentSpecies::Blue];
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-struct Agent {
-    id: u32,
-    node_index: u32,
-    species: AgentSpecies,
-}
-
 #[derive(Debug, Clone)]
 struct Node {
-    index: u32,
     pub neighbours: Vec<u32>, // indices of neighbours
     pub grafitti: HashMap<AgentSpecies, f32>,
     pub pull_strength: HashMap<AgentSpecies, f32>,
+    pub blue_agents: u32,
+    pub red_agents: u32,
 }
 
 impl Node {
     pub fn new(index: u32, edges: &HashMap<u32, Vec<u32>>) -> Node {
         Node {
-            index,
             neighbours: edges.get(&index).unwrap().to_vec(),
             grafitti: HashMap::new(),
             pull_strength: HashMap::new(),
+            blue_agents: 0,
+            red_agents: 0,
         }
     }
 
-    pub fn agents_with_species(
-        &self,
-        agents: &HashMap<u32, Vec<Agent>>,
-        species: AgentSpecies,
-    ) -> u32 {
-        agents
-            .get(&self.index)
-            .unwrap()
-            .iter()
-            .filter(|agent| agent.species == species)
-            .count() as u32
+    pub fn add_agents(&mut self, amount: u32, species: AgentSpecies) {
+        match species {
+            AgentSpecies::Red => self.red_agents += amount,
+            AgentSpecies::Blue => self.blue_agents += amount,
+        }
     }
 
-    pub fn update_grafitti_and_pull(&mut self, agents: &Vec<HashSet<Agent>>, grid_size: u32) {
+    pub fn agents_with_species(&self, species: &AgentSpecies) -> u32 {
+        match species {
+            AgentSpecies::Red => self.red_agents,
+            AgentSpecies::Blue => self.blue_agents,
+        }
+    }
+
+    pub fn update_grafitti_and_pull(&mut self, grid_size: u32) {
         for species in &AGENT_SPECIES {
+            let num_agents_of_species = self.agents_with_species(species);
+
             let entry: &mut f32 = self.grafitti.entry(*species).or_insert(0.0);
 
             // 0 - Decrement current grafitti by lambda
             *entry *= HYPER_PARAMS.lambda;
-
-            // TODO: optimise this operation to O(1) instead of O(n)
-            let num_agents_of_species = agents
-                .get(self.index as usize)
-                .unwrap()
-                .iter()
-                .filter(|agent| agent.species == *species)
-                .count() as f32;
 
             // 1 - Increase grafiti by gamma * sum of same agent' count
             *entry += HYPER_PARAMS.gamma * num_agents_of_species as f32;
@@ -92,7 +78,6 @@ impl Node {
 pub struct Universe2D {
     size: u32,
     nodes: Vec<Node>,
-    agents: Vec<HashSet<Agent>>,
     prng: ChaCha8Rng,
     iteration: u32,
 }
@@ -124,32 +109,25 @@ impl Universe2D {
             }
         }
 
-        let nodes: Vec<Node> = (0..(size * size))
+        let mut nodes: Vec<Node> = (0..(size * size))
             .map(|index| Node::new(index, &edges))
             .collect();
 
-        let mut agents: Vec<HashSet<Agent>> = vec![HashSet::new(); (size * size) as usize];
-
         (0..agent_size * 2).for_each(|id| {
             let node_index = prng.gen_range(0..(size * size));
-            let agent = Agent {
-                id,
-                node_index,
-                species: if id % 2 == 0 {
-                    AgentSpecies::Red
-                } else {
-                    AgentSpecies::Blue
-                },
+            let species = if id % 2 == 0 {
+                AgentSpecies::Red
+            } else {
+                AgentSpecies::Blue
             };
 
-            agents[node_index as usize].insert(agent);
+            nodes[node_index as usize].add_agents(1, species);
         });
 
         Universe2D {
             size,
             nodes,
             prng,
-            agents,
             iteration: 0,
         }
     }
@@ -159,34 +137,49 @@ impl Universe2D {
     pub fn tick(&mut self) {
         // 0) update grafitti in nodes
         for node in &mut self.nodes {
-            node.update_grafitti_and_pull(&self.agents, self.size);
+            node.update_grafitti_and_pull(self.size);
         }
 
-        // 2) move agents
-        let mut new_agents: Vec<HashSet<Agent>> =
-            vec![HashSet::new(); (self.size * self.size) as usize];
+        // 1) move agents out of nodes
+        let mut new_nodes = self.nodes.clone();
+        // reset agents in nodes
+        for node in &mut new_nodes {
+            node.red_agents = 0;
+            node.blue_agents = 0;
+        }
 
-        for (node_idx, hash_agents) in self.agents.iter_mut().enumerate() {
+        for node in &self.nodes {
             for species in &AGENT_SPECIES {
-                let neighbours = &self.nodes[node_idx].neighbours;
-
-                let neighbour_strengths = neighbours
+                let neighbours = &node.neighbours;
+                let _neighbour_strengths = neighbours
                     .iter()
                     .map(|neighbour_idx| {
                         self.nodes[*neighbour_idx as usize]
                             .pull_strength
-                            .get(species)
+                            .get(&AgentSpecies::Red)
                             .unwrap_or(&0.0)
                     })
                     .collect::<Vec<&f32>>();
 
-                for agent in hash_agents.iter() {
-                    let new_node_index = self.prng.gen_range(0..neighbours.len()); // TODO: make this biased
+                let mut new_agents: HashMap<u32, u32> = HashMap::new(); // HashMap with key: node_index, value: num_agents
+                neighbours.iter().for_each(|idx| {
+                    new_agents.insert(*idx, 0);
+                });
 
-                    new_agents[new_node_index].insert(agent.clone());
+                for _ in 0..node.agents_with_species(species) {
+                    let new_node_index: u32 = self.prng.gen_range(0..neighbours.len()) as u32;
+
+                    let neighbour_index = neighbours[new_node_index as usize];
+                    *new_agents.get_mut(&neighbour_index).unwrap() += 1;
                 }
+
+                new_agents.iter().for_each(|(node_index, amount)| {
+                    new_nodes[*node_index as usize].add_agents(*amount, *species)
+                });
             }
         }
+
+        self.nodes = new_nodes;
 
         self.iteration += 1;
     }
@@ -198,7 +191,6 @@ impl fmt::Display for Universe2D {
 
         write!(f, "size: {}\n", self.size)?;
         write!(f, "node size: {}\n", self.nodes.len())?;
-        write!(f, "agent size: {}\n", self.agents.len())?;
         write!(f, "iterations: {}\n", self.iteration)?;
 
         write!(f, "{}\n", "=".repeat(30))?;
@@ -207,18 +199,16 @@ impl fmt::Display for Universe2D {
                 let index = y * self.size + x;
                 let node = &self.nodes[index as usize];
 
-                let blue_agents = self.agents[index as usize]
-                    .iter()
-                    .filter(|agent| agent.species == AgentSpecies::Blue)
-                    .count();
+                let blue_agents =
+                    self.nodes[index as usize].agents_with_species(&AgentSpecies::Blue);
+                let red_agents = self.nodes[index as usize].agents_with_species(&AgentSpecies::Red);
 
-                let red_agents = self.agents[index as usize].iter().count() - blue_agents;
                 let blue_graffiti = node.grafitti.get(&AgentSpecies::Blue).unwrap_or(&0.0);
                 let red_graffiti = node.grafitti.get(&AgentSpecies::Red).unwrap_or(&0.0);
 
                 write!(
                     f,
-                    "| i:{}|b:{},r:{}|g:({},{})",
+                    "|{} a({},{}) g:({},{})",
                     index.to_string().with_exact_width(2),
                     blue_agents.to_string().with_exact_width(2),
                     red_agents.to_string().with_exact_width(2),
@@ -233,34 +223,29 @@ impl fmt::Display for Universe2D {
 }
 
 mod test {
+    use std::time::Instant;
+
     use super::*;
 
     fn total_agent_size(universe: &Universe2D) -> u32 {
         universe
-            .agents
+            .nodes
             .iter()
-            .map(|hash_agents| hash_agents.len() as u32)
+            .map(|node| node.blue_agents + node.red_agents)
             .sum()
     }
 
     fn total_agent_size_of_species(universe: &Universe2D, species: AgentSpecies) -> u32 {
         universe
-            .agents
+            .nodes
             .iter()
-            .map(|hash_agents| {
-                hash_agents
-                    .iter()
-                    .filter(|agent| agent.species == species)
-                    .count() as u32
-            })
+            .map(|node| node.agents_with_species(&species))
             .sum()
     }
 
     #[test]
     fn test_universe2d() {
         let universe = Universe2D::new(4, 100);
-
-        assert_eq!(universe.agents.len(), 16);
 
         for node in &universe.nodes {
             // FOR DEBUG println!("{:?}", node);
@@ -284,16 +269,37 @@ mod test {
     #[test]
     fn test_tick() {
         let mut universe = Universe2D::new(4, 100);
-        universe.tick();
-        println!("{}", universe);
-        universe.tick();
+
+        for _ in 0..10 {
+            universe.tick();
+        }
         println!("{}", universe);
 
         // all notes should have a grafitti of >0.0
         for node in &universe.nodes {
             for species in &AGENT_SPECIES {
-                assert_eq!(node.grafitti.contains_key(species), true);
-                assert_eq!(node.grafitti.get(species).unwrap() > &0.0, true);
+                assert_eq!(node.grafitti.contains_key(species), true); // TODO
+                assert_eq!(node.grafitti.get(species).unwrap() > &0.0, true); // TODO
+            }
+        }
+    }
+
+    #[test]
+    fn performance_test_tick() {
+        let mut universe = Universe2D::new(100, 100000);
+        let start = Instant::now();
+
+        for _ in 0..10 {
+            universe.tick();
+        }
+
+        println!("{:?}", start.elapsed());
+
+        // all notes should have a grafitti of >0.0
+        for node in &universe.nodes {
+            for species in &AGENT_SPECIES {
+                assert_eq!(node.grafitti.contains_key(species), true); // TODO
+                assert_eq!(node.grafitti.get(species).unwrap() > &0.0, true); // TODO
             }
         }
     }
